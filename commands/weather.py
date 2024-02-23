e1817e2e31d4159abf158b252c8d9bfd145bf53e
1lamafarmer <140810532+Lucaswatt@users.noreply.github.com>
2024-02-23 18:26:46 +1300

    i am committed to committing

--- commands/weather.py
+++ commands/weather.py
@@ -1,3 +1,5 @@
+import datetime
+
 import nextcord
 from loggerthyst import info, warn, error, fatal
 from nextcord.ext import commands
@@ -9,18 +11,25 @@ class Weather(commands.Cog):
         self.bot = bot
 
     # Weather
-    @nextcord.slash_command(description="Get the latency from the bot to Discords servers")
-    async def weather(self, interaction: nextcord.Interaction, lat: float = 40.9006, long: float = 174.8860, ):
+    @nextcord.slash_command(description="Get the weather forecast for the next 24 hours")
+    async def hourly_forecast(self, interaction: nextcord.Interaction, lat: float = 40.9006, long: float = 174.8860):
         # fetch api stuff
-        raw = httpx.get(f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&hourly=temperature_2m&timezone=Pacific%2FAuckland&forecast_days=14")
+        raw = httpx.get(f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,rain&timezone=Pacific%2FAuckland&forecast_days=1")
         data = raw.json()
 
-        temperature_2m = data["hourly"]["temperature_2m"]
         timestamps = data["hourly"]["time"]
+        temperature = data["hourly"]["temperature_2m"]
+        humidity = data["hourly"]["relative_humidity_2m"]
+        dewpoint = data["hourly"]["dew_point_2m"]
+        rainfall = data["hourly"]["rain"]
+        elevation = data["elevation"]
 
-        image = graph.weather_graph(timestamps, temperature_2m)
+        image = graph.weather_graph(timestamps, temperature, humidity, dewpoint, rainfall)
 
-        embed = nextcord.Embed(title=f"Temperature data", color=0x3346D1)
+        embed = nextcord.Embed(title=f"24-hour forecast", description="Data pulled from `open-meteo.com`", color=0x3346D1)
+        embed.add_field(name="Latitude", value=f"{lat}° S", inline=True)
+        embed.add_field(name="Longitude", value=f"{long}° E", inline=True)
+        embed.add_field(name="Elevation", value=f"{elevation}m", inline=True)
         embed.set_image(url="attachment://file.png")
 
         await interaction.response.send_message(embed=embed, file=nextcord.File(image, filename="file.png"))
--- helpers/graph.py
+++ helpers/graph.py
@@ -7,13 +7,19 @@ import io
 def format_y_tick(value, pos):
     return f'{value}c'
 
-def weather_graph(timestamps, ydata):
+def weather_graph(timestamps, temperature, humidity, dewpoint, rainfall):
     mpl.rcParams['savefig.pad_inches'] = 0
-    ax = plt.axes()
+    ax = plt.axes(frameon=False)
     plt.autoscale(tight=True)
-    plt.ylim(min(ydata) - 1, max(ydata) + 1)
+    plt.ylim(0, 100)
+    plt.grid(color='gray', linestyle='-', linewidth=0.5, alpha=0.5)
 
-    ax.plot(timestamps, ydata)
+    ax.plot(timestamps, temperature, 'o-', label="Temperature (°C)")
+    ax.plot(timestamps, humidity, 'o-', label="Humidity (%)")
+    ax.plot(timestamps, dewpoint, 'o-', label="Dew point (°C)")
+    ax.plot(timestamps, rainfall, 'o-', label="Rainfall (mm)")
+
+    ax.legend()
 
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
@@ -23,9 +29,11 @@ def weather_graph(timestamps, ydata):
     ax.tick_params(axis='x', colors='white')
     ax.tick_params(axis='y', colors='white')
 
-    plt.xticks(ticks=timestamps, labels=[datetime.strptime(ts, "%Y-%m-%dT%H:%M").strftime("%b %d") if index % 24 == 0 else None for index, ts in enumerate(timestamps)], rotation=45)
+    plt.xticks(ticks=timestamps, labels=[datetime.strptime(ts, "%Y-%m-%dT%H:%M").strftime("%H:%M") for index, ts in enumerate(timestamps)], rotation=45)
+    plt.yticks(range(0, 101, 10))
 
-    ax.yaxis.set_major_formatter(ticker.FuncFormatter(format_y_tick))
+    ax.xaxis.set_major_locator(plt.MaxNLocator(12))
+    # ax.yaxis.set_major_formatter(ticker.FuncFormatter(format_y_tick))
 
     img_bytes = io.BytesIO()
     plt.savefig(img_bytes, format='png', transparent=True)
