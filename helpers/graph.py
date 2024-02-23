63a90295eb7a3b1bffefa3fb1316a27dc34cd5d9
1lamafarmer <140810532+Lucaswatt@users.noreply.github.com>
2024-02-23 14:48:47 +1300

    e

--- helpers/graph.py
+++ helpers/graph.py
@@ -1,17 +1,20 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import io
 
-def graph(title: str, data: list[float]):
-    x_values = list(range(1, len(data) + 1))
-    plt.plot(x_values, data)
+def graph(xdata, ydata):
+    mpl.rcParams['savefig.pad_inches'] = 0
 
-    plt.xlabel('Data Points')
-    plt.ylabel('Values')
+    ax = plt.axes((0, 0, 1, 1), frameon=False)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.autoscale(tight=True)
 
-    plt.title(title)
+    # Plot the data.
+    plt.plot(ydata)
 
     img_bytes = io.BytesIO()
-    plt.savefig(img_bytes, format='png')
+    plt.savefig(img_bytes, format='png', transparent=True)
     img_bytes.seek(0)
 
     plt.clf()
