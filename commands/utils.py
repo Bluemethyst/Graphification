382748f8b5ffb4a6611977252748b7766b361b08
blue <bluemethyst@gmail.com>
2024-02-23 13:22:27 +1300

    ping command functioning

--- commands/utils.py
+++ commands/utils.py
@@ -1,17 +1,14 @@
 import nextcord
-from .. import loggerthyst
 from loggerthyst import info, warn, error, fatal
 from nextcord.ext import commands
 
 
-class Ping(commands.Cog):
+class Utils(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     # PING
-    @nextcord.slash_command(
-        description="Get the latency from the bot to Discords servers"
-    )
+    @nextcord.slash_command(name="ping", description="Check the bots latencys")
     async def ping(self, interaction: nextcord.Interaction):
         latency = round(self.bot.latency * 1000, 0)
         embed = nextcord.Embed(title=f"Latency: {latency}MS", color=0x3346D1)
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import dotenv
 import os
 import nextcord
-from commands.ping import Ping
+from commands.utils import Utils
 from loggerthyst import error, info, fatal, warn
 from nextcord.ext import commands
 
-
-bot = commands.Bot()
-bot.add_cog(Ping(bot))
 dotenv.load_dotenv()
+intents = nextcord.Intents.all()
+bot = commands.Bot(intents=intents)
+bot.add_cog(Utils(bot))
 
 
 @bot.event
