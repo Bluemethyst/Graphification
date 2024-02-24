6808b9857582f88b004949dd086ce202f175e8f8
1lamafarmer <140810532+Lucaswatt@users.noreply.github.com>
2024-02-24 13:35:01 +1300

    documentationibleisms

--- commands/weather.py
+++ commands/weather.py
@@ -1,3 +1,4 @@
+# @formatter:off
 import nextcord
 from loggerthyst import info, warn, error, fatal
 from nextcord.ext import commands
@@ -10,44 +11,32 @@ class Weather(commands.Cog):
         self.bot = bot
 
     # WEATHER
-    @nextcord.slash_command(
-        description="Get the weather forecast for the next 24 hours"
-    )
-    async def hourly_forecast(
-        self,
-        interaction: nextcord.Interaction,
-        lat: float = 40.9006,
-        long: float = 174.8860,
-    ):
+    @nextcord.slash_command(description="Get the weather forecast for the next 24 hours")
+    async def hourly_forecast(self, interaction: nextcord.Interaction, lat: float = 40.9006, long: float = 174.8860):
         await interaction.response.defer()
-        # fetch api stuff
+
         raw = httpx.get(
-            f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,rain&timezone=Pacific%2FAuckland&forecast_days=1"
-        )
+            f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,precipitation,rain,showers,snowfall&forecast_days=1")
         data = raw.json()
 
         timestamps = data["hourly"]["time"]
         temperature = data["hourly"]["temperature_2m"]
-        humidity = data["hourly"]["relative_humidity_2m"]
+        apparent_temperature = data["hourly"]["apparent_temperature"]
         dewpoint = data["hourly"]["dew_point_2m"]
+        humidity = data["hourly"]["relative_humidity_2m"]
+        precipitation = data["hourly"]["precipitation"]
         rainfall = data["hourly"]["rain"]
+        showers = data["hourly"]["showers"]
+        snowfall = data["hourly"]["snowfall"]
         elevation = data["elevation"]
 
-        image = graph.weather_graph(
-            timestamps, temperature, humidity, dewpoint, rainfall
-        )
+        image = graph.weather_graph(timestamps, temperature, humidity, dewpoint, rainfall)
 
-        embed = nextcord.Embed(
-            title=f"24-hour forecast",
-            description="Data pulled from `open-meteo.com`",
-            color=0x3346D1,
-        )
+        embed = nextcord.Embed(title=f"24-hour forecast", description="Data pulled from `open-meteo.com`", color=0x3346D1)
         embed.add_field(name="Latitude", value=f"{lat}° S", inline=True)
         embed.add_field(name="Longitude", value=f"{long}° E", inline=True)
         embed.add_field(name="Elevation", value=f"{elevation}m", inline=True)
         embed.set_image(url="attachment://file.png")
 
-        await interaction.followup.send(
-            embed=embed, file=nextcord.File(image, filename="file.png")
-        )
+        await interaction.followup.send(embed=embed, file=nextcord.File(image, filename="file.png"))
         info(command="Weather", interaction=interaction)
--- helpers/graph.py
+++ helpers/graph.py
@@ -4,13 +4,55 @@ import matplotlib.ticker as ticker
 from datetime import datetime
 import io
 
+
+def template_graph(x_axis, y_axis):
+    mpl.rcParams['savefig.pad_inches'] = 0  # Remove 0.1in border around graph
+    plt.figure(figsize=(6.4, 3.2))  # Set the size of the graph (in inches - width x height)
+    ax = plt.axes(frameon=False)  # Initialize axis
+    plt.autoscale(tight=True)  # Enable tight autoscaling
+    plt.ylim(0, 100)  # Lock the Y axis between 2 values (not necessary)
+    plt.grid(color='gray', linestyle='-', linewidth=0.5, alpha=0.5)  # Set up a light grey grid behind the data
+
+    ax.plot(x_axis, y_axis, 'o-', label="Temperature (°C)")  # Plot the data. 'o-' is the line style. label is used in the legend
+    # You can duplicate the line above to create multiple lines
+
+    ax.legend()  # Create a legend using the label(s) defined above. Not necessary for one line graphs
+
+    ax.spines['top'].set_visible(False)  # Hide the top spline
+    ax.spines['right'].set_visible(False)  # Hide the right spline
+    ax.spines['left'].set_color('white')  # Set colour of left spine
+    ax.spines['bottom'].set_color('white')  # Set colour of bottom spine
+
+    ax.tick_params(axis='x', colors='white')  # Set the colour of the ticks on the X axis
+    ax.tick_params(axis='y', colors='white')  # Set the colour of the ticks on the Y axis
+
+    plt.xticks(ticks=timestamps,
+               labels=[datetime.strptime(ts, "%Y-%m-%dT%H:%M").strftime("%H:%M") for index, ts in enumerate(timestamps)],
+               rotation=45)  # Initialize the x ticks using the timestamps list. The 'labels' section is what is actually displayed, so is parsed here.
+
+    plt.yticks(range(0, 101, 10))  # This sets the y ticks to clean intervals of 10
+
+    ax.xaxis.set_major_locator(plt.MaxNLocator(12))  # Limit the maximum number of ticks on the X axis. This is very important for clean graphs
+    # ax.yaxis.set_major_formatter(ticker.FuncFormatter(format_y_tick))  # You can specify a formatter function for the Y axis here
+
+    img_bytes = io.BytesIO()  # Set up a BytesIO object to store the image in
+    plt.savefig(img_bytes, format='png', transparent=True)  # Save the image. transparent=True is important for discord embeds
+    img_bytes.seek(0)  # Idk what this does tbh but ChatGPT told me to put it here
+
+    plt.clf()  # Clear the graph for some reason? ig it's already saved
+
+    return img_bytes  # Return the image
+
+
 def format_y_tick(value, pos):
     return f'{value}c'
 
-def weather_graph(timestamps, temperature, humidity, dewpoint, rainfall):
-    mpl.rcParams['savefig.pad_inches'] = 0
-    ax = plt.axes(frameon=False)
-    plt.autoscale(tight=True)
+
+def hourly_weather_graph(timestamps, temperature, humidity, dewpoint, rainfall):
+    mpl.rcParams['savefig.pad_inches'] = 0  # Remove 0.1in border around graph
+    plt.figure(figsize=(6.4, 3.2))  # Set the size of the graph (in inches - width x height)
+    ax = plt.axes(frameon=False)  # Initialize axis
+    plt.autoscale(tight=True)  # Enable tight autoscaling
     plt.ylim(0, 100)
     plt.grid(color='gray', linestyle='-', linewidth=0.5, alpha=0.5)
 
@@ -29,7 +71,8 @@ def weather_graph(timestamps, temperature, humidity, dewpoint, rainfall):
     ax.tick_params(axis='x', colors='white')
     ax.tick_params(axis='y', colors='white')
 
-    plt.xticks(ticks=timestamps, labels=[datetime.strptime(ts, "%Y-%m-%dT%H:%M").strftime("%H:%M") for index, ts in enumerate(timestamps)], rotation=45)
+    plt.xticks(ticks=timestamps, labels=[datetime.strptime(ts, "%Y-%m-%dT%H:%M").strftime("%H:%M") for index, ts in
+                                         enumerate(timestamps)], rotation=45)
     plt.yticks(range(0, 101, 10))
 
     ax.xaxis.set_major_locator(plt.MaxNLocator(12))
